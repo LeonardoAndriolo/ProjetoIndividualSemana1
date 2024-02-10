605879e071455f71c42108e04bb45ed0dc646545
LeoAndriolo <leonardo.andriolo@sou.inteli.edu.br>
2024-02-10 11:15:03 -0300

    Add music file and code logic

--- exercicio_peixe/peixe.js
+++ exercicio_peixe/peixe.js
@@ -25,22 +25,24 @@ function preload() {
 
     this.load.image('bubbles', 'assets/bubbles.png');
 
+    this.load.audio("main_theme", "assets/audio/water_prelude.mp3");
+
 }
 
 function create() {
+    // Load music
+    const main_theme = this.sound.add("main_theme", { volume: 0.4 });
+    main_theme.play();
+
+    // Load images
     this.add.image(400, 300, 'mar').setScale(1.6);
-
     this.add.image(330, 400, 'poseidon').setScale(0.6);
-
     this.add.image(680, 60, 'logo').setScale(0.6);
-
     bubbles = this.add.image(500, 300, 'bubbles').setScale(0.1);
-    
     peixinho = this.add.image(400, 300, 'peixe')
                         .setScale(0.2)
                         .setOrigin(0.5, 0.5);
-    
-    peixinho.setFlip(true, false);
+    peixinho.setFlip(true, false); // Set fish orientation
 }
 
 function update() {
